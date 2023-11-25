712861a99c8aa33378a68eed2aa376beed9ecd27
harsh <harsh@HARSHPREETKAUR>
2023-11-25 01:04:49 -0500

    Add project files.

---
+++ Looping assignment/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        MenuStrip1 = New MenuStrip()
+        LinesOvlpdToolStripMenuItem = New ToolStripMenuItem()
+        CurvedLinesOvlpdToolStripMenuItem = New ToolStripMenuItem()
+        DiamondToolStripMenuItem = New ToolStripMenuItem()
+        GridToolStripMenuItem1 = New ToolStripMenuItem()
+        GridToolStripMenuItem = New ToolStripMenuItem()
+        CirclesOvlpdToolStripMenuItem = New ToolStripMenuItem()
+        ClearToolStripMenuItem = New ToolStripMenuItem()
+        Button8 = New Button()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(26, 526)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "lines ovlpd"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(107, 526)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(122, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "curved lines ovlpd"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(235, 526)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 2
+        Button3.Text = "diamond"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(316, 526)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 3
+        Button4.Text = "squares"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(397, 526)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 23)
+        Button5.TabIndex = 4
+        Button5.Text = "spiral"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(478, 526)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(92, 23)
+        Button6.TabIndex = 5
+        Button6.Text = "circles ovlpd"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(745, 526)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 23)
+        Button7.TabIndex = 6
+        Button7.Text = "clear"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {LinesOvlpdToolStripMenuItem, CurvedLinesOvlpdToolStripMenuItem, DiamondToolStripMenuItem, GridToolStripMenuItem1, GridToolStripMenuItem, CirclesOvlpdToolStripMenuItem, ClearToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(884, 24)
+        MenuStrip1.TabIndex = 7
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' LinesOvlpdToolStripMenuItem
+        ' 
+        LinesOvlpdToolStripMenuItem.Name = "LinesOvlpdToolStripMenuItem"
+        LinesOvlpdToolStripMenuItem.Size = New Size(76, 20)
+        LinesOvlpdToolStripMenuItem.Text = "lines ovlpd"
+        ' 
+        ' CurvedLinesOvlpdToolStripMenuItem
+        ' 
+        CurvedLinesOvlpdToolStripMenuItem.Name = "CurvedLinesOvlpdToolStripMenuItem"
+        CurvedLinesOvlpdToolStripMenuItem.Size = New Size(115, 20)
+        CurvedLinesOvlpdToolStripMenuItem.Text = "curved lines ovlpd"
+        ' 
+        ' DiamondToolStripMenuItem
+        ' 
+        DiamondToolStripMenuItem.Name = "DiamondToolStripMenuItem"
+        DiamondToolStripMenuItem.Size = New Size(67, 20)
+        DiamondToolStripMenuItem.Text = "diamond"
+        ' 
+        ' GridToolStripMenuItem1
+        ' 
+        GridToolStripMenuItem1.Name = "GridToolStripMenuItem1"
+        GridToolStripMenuItem1.Size = New Size(40, 20)
+        GridToolStripMenuItem1.Text = "grid"
+        ' 
+        ' GridToolStripMenuItem
+        ' 
+        GridToolStripMenuItem.Name = "GridToolStripMenuItem"
+        GridToolStripMenuItem.Size = New Size(47, 20)
+        GridToolStripMenuItem.Text = "spiral"
+        ' 
+        ' CirclesOvlpdToolStripMenuItem
+        ' 
+        CirclesOvlpdToolStripMenuItem.Name = "CirclesOvlpdToolStripMenuItem"
+        CirclesOvlpdToolStripMenuItem.Size = New Size(85, 20)
+        CirclesOvlpdToolStripMenuItem.Text = "circles ovlpd"
+        ' 
+        ' ClearToolStripMenuItem
+        ' 
+        ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        ClearToolStripMenuItem.Size = New Size(44, 20)
+        ClearToolStripMenuItem.Text = "clear"
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(576, 526)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 23)
+        Button8.TabIndex = 8
+        Button8.Text = "Button8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(884, 561)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents LinesOvlpdToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CurvedLinesOvlpdToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DiamondToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GridToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CirclesOvlpdToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GridToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents Button8 As Button
+End Class
---
+++ Looping assignment/Form1.vb
@@ -0,0 +1,191 @@
+﻿Imports System.Reflection
+
+Public Class Form1
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim formsurface As Graphics = CreateGraphics()
+        formsurface.Clear(BackColor)
+        For y = 0 To 490 Step 10
+            formsurface.DrawLine(Pens.Green, 0, 0, 940, y)
+        Next
+
+        For y = 0 To 460 Step 10
+            formsurface.DrawLine(Pens.Blue, 900, 0, 0, y)
+        Next
+
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim g As Graphics = CreateGraphics()
+        g.Clear(BackColor)
+        For x = 0 To 600 Step 10
+            g.DrawLine(Pens.Red, 300, 0, x, 250)
+        Next
+
+        For x = 0 To 600 Step 10
+            g.DrawLine(Pens.Red, 300, 500, x, 250)
+        Next
+
+    End Sub
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim g As Graphics = CreateGraphics()
+        Dim y, x As Integer
+        g.Clear(BackColor)
+        Do While y <= 900
+
+            g.DrawLine(Pens.DarkOrchid, 0, y, 900, y)
+            y = y + 20
+        Loop
+
+        Do While x <= 900
+
+            g.DrawLine(Pens.DarkOrchid, x, 0, x, 900)
+            x = x + 20
+        Loop
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Dim g As Graphics = CreateGraphics()
+
+        Dim x, width, height As Integer
+        g.Clear(BackColor)
+        For x = 0 To 560 Step 15
+            width = x
+            height = x
+
+            g.DrawEllipse(Pens.DarkViolet, 0, 0, width, height)
+
+        Next
+
+
+
+        'Dim x, y As Integer
+
+        'For x = 0 To 100 Step 20
+        '    For y = 0 To 100 Step 20
+        '        g.DrawEllipse(Pens.DarkViolet, 0, 0, 100 - x, 100 - y)
+
+        '    Next
+
+        'Next
+
+
+
+
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Dim g As Graphics = CreateGraphics()
+        Dim x, y As Integer
+        g.Clear(BackColor)
+        Do While x <= 800
+            g.DrawEllipse(Pens.DarkTurquoise, x, 0, 200, 200)
+            x = x + 100
+        Loop
+
+        Do While y <= 800
+            g.DrawEllipse(Pens.DarkTurquoise, y, 400, 200, 200)
+            y = y + 100
+        Loop
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim g As Graphics = CreateGraphics()
+        g.Clear(BackColor)
+    End Sub
+
+    Private Sub GridToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GridToolStripMenuItem.Click
+        Dim g As Graphics = CreateGraphics()
+
+        Dim x, width, height As Integer
+
+        For x = 0 To 560 Step 15
+            width = x
+            height = x
+
+            g.DrawEllipse(Pens.DarkViolet, 0, 0, width, height)
+
+        Next
+    End Sub
+
+    Private Sub LinesOvlpdToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LinesOvlpdToolStripMenuItem.Click
+
+        Dim formsurface As Graphics = CreateGraphics()
+
+        For y = 0 To 490 Step 10
+            formsurface.DrawLine(Pens.Green, 0, 0, 940, y)
+        Next
+
+        For y = 0 To 460 Step 10
+            formsurface.DrawLine(Pens.Blue, 900, 0, 0, y)
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+
+
+
+    'g.DrawLine(Pens.Purple, 0, 0, 20, 500)
+    'g.DrawLine(Pens.Purple, 0, 20, 40, 500)
+    'g.DrawLine(Pens.Purple, 0, 40, 60, 500)
+    'g.DrawLine(Pens.Purple, 0, 60, 80, 500)
+    'g.DrawLine(Pens.Purple, 0, 80, 100, 500)
+    'g.DrawLine(Pens.Purple, 0, 100, 120, 500)
+    'g.DrawLine(Pens.Purple, 0, 120, 140, 500)
+
+    'Do While x <= 880
+    'Do While y <= 480
+    '            g.DrawLine(Pens.Purple, 0, y, x, 500)
+    '            x = x + 20
+    '            y = y + 20
+    '        Loop
+    '    Loop
+    'End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+
+        Dim g As Graphics = CreateGraphics()
+        Dim x, y, x1, y2 As Integer
+        Dim rand As New Random()
+        'For x1 = -15 To 900 Step 50
+        '    g.DrawRectangle(Pens.Purple, x * 500, -15, 30, 30)
+
+        'For x = -15 To 900 Step 50
+        '    For y = -15 To 500 Step 50
+        '        g.DrawRectangle(Pens.Purple, x, y, 30, 30)
+        '    Next
+
+        'Next
+
+        'For x1 = 20 To 900 Step 50
+        g.Clear(BackColor)
+        For x = -15 To 900 Step 50
+            For y = -15 To 500 Step 50
+                Dim r As Integer = rand.Next(0, 256)
+                Dim gr As Integer = rand.Next(0, 256)
+                Dim b As Integer = rand.Next(0, 256)
+                Dim brush As New Pen(Color.FromArgb(r, gr, b))
+                g.DrawRectangle(brush, x, y, 30, 30)
+            Next
+        Next
+
+
+
+    End Sub
+End Class
